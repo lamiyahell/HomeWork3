cb68681c862a281cc1c58e94d558e0a79d3a8070
lamiyahell <axira1108@gmail.com>
2023-03-28 21:46:41 +0300

    Задачи 4-7 выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Условый оператор. Домашние задания");
@@ -24,6 +26,59 @@ public class Main {
             System.out.println("Если скорость " + speedCar + ", придется заплатить штраф");
         } else {
             System.out.println("Если скорость " + speedCar + ", можно ездить спокойно");
+        }
+        //task 4
+        System.out.println("Задача 4");
+        int ageHuman = 4;
+        boolean kinderGarden = ageHuman >= 2 && ageHuman < 6;
+        boolean pupilSchool = ageHuman >= 7 && ageHuman < 18;
+        boolean student = ageHuman >= 18 && ageHuman < 24;
+        boolean worker = ageHuman > 24;
+        if (kinderGarden) {
+            System.out.println("Если возраст человека равен " + ageHuman + ", то ему нужно ходить в детский сад");
+        } else if (pupilSchool) {
+            System.out.println("Если возраст человека равен " + ageHuman + ", то ему нужно ходить в школу");
+        } else if (student) {
+            System.out.println("Если возраст человека равен " + ageHuman + ", то ему нужно ходить в университет");
+        } else if (worker) {
+            System.out.println("Если возраст человека равен " + ageHuman + ", то ему нужно ходить на работу");
+        }
+
+        //task 5
+        System.out.println("Задача 5");
+        int ageChild = 16;
+        if (ageChild < 5) {
+            System.out.println("Если возраст ребенка равен " + ageChild + ", то ему нельзя кататься на аттракционе");
+        } else if (ageChild >= 5 && ageChild < 14) {
+            System.out.println("Если возраст человека равен " + ageChild + ", то ему можно кататься на аттракционе в сопровождении взрослого");
+        } else if (ageChild >= 14) {
+            System.out.println("Если возраст человека равен " + ageChild + ", то ему можно кататься на аттракционе без сопровождения взрослого");
+        }
+
+        //task 6
+        System.out.println("Задача 6");
+        int wagonCapacity = 102;
+        int seatsInTheWagon = 60;
+        int people = 104;
+        if (people < seatsInTheWagon) {
+            System.out.println("В вагоне есть место и можно занять сидячее место");
+        } else if (people >= seatsInTheWagon && people < wagonCapacity) {
+            System.out.println("В вагоне есть место, но только стоячее");
+        } else if (people > 102) {
+            System.out.println("Вагон уже полностью забит, мест нет");
+        }
+
+
+        //task 7
+        System.out.println("Задача 7");
+        int one;
+        int two;
+        int three;
+        one = 1;
+        two = 2;
+        three = 3;
+        if (one < two && one < three && two < three) {
+            System.out.println("Из трёх чисел большее " + three);
         }
     }
 }
